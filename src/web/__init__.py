c7123bc7abd66720a73b4869105607b21d61b237
YutaKoyanagi10 <yuta.koyanagi@alu.ing.unlp.edu.ar>
2026-05-09 01:45:38 -0300

    Implementación del entorno de desarrollo y configuración inicial del proyecto

---
+++ src/web/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask import render_template
+
+def create_app(env="development", static_folder="../../static"):
+    app = Flask(__name__, static_folder=static_folder)
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+    return app
