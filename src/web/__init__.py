a798d07a96f540706afa18d0986ef9f7ca11a7d1
Ian Kieferling <iankieferling@gmail.com>
2026-05-10 16:38:58 -0300

    Conexion con el modulo de autenticacion

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
+from src.web.controllers.auth import auth_bp
 
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
+    app.register_blueprint(auth_bp)
     @app.route("/")
     def home():
         return render_template("index.html")
