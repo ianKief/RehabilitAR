c8d066f8de4e76b00010ff8b1595841b0cd27271
YutaKoyanagi10 <yuta.koyanagi@alu.ing.unlp.edu.ar>
2026-05-18 17:45:17 -0300

    crud de salas

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, request,render_template
 from src.web.config import config
-from src.core.database import db, init_db, reset_db, seed_db
+from src.web.handlers import error
+from src.core.database import init_db, reset_db, seed_db
 from src.web.controllers.salas import bp as salas_bp
 
 def create_app():
@@ -25,6 +26,12 @@ def create_app():
     def seed_db_command():
         """Pobla la base de datos con datos de prueba."""
         seed_db()
+
+    # Registrar manejadores de errores
+    app.register_error_handler(404, error.not_found)
+    app.register_error_handler(401, error.unauthorized)
+    app.register_error_handler(403, error.forbidden)
+    app.register_error_handler(500, error.internal_server_error)
 
     @app.route("/")
     def home():
