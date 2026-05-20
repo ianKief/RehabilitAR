c4970c094c9b22f1c56ff4f84b812da45b0df1b6
Ian Kieferling <iankieferling@gmail.com>
2026-05-20 18:03:26 -0300

    Validaciones modificadas + logica para listar usuarios

--- src/web/controllers/usuarios.py
+++ src/web/controllers/usuarios.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from src.core.usuarios import crear_usuario, listar_usuarios
 
 users_bp = Blueprint('usuarios', __name__, url_prefix='/usuarios')
 
@@ -14,27 +15,27 @@ def crear_usuario():
         # Validaciones
         # 1. Validar que no falte ningun campo obligatorio
         if not all([nombre, email, password, rol]):
-            # Si falta algun campo obligatorio, podriamos mostrar un mensaje de error
             return render_template('usuarios/crear.html', error="Por favor, complete todos los campos obligatorios.")
-        
-        # 2. Validar que el email no exista en la base de datos
-        # ...
-        
-        # 3. Validar que el password tenga al menos 6 caracteres
+
+        # 2. Validar que el password tenga al menos 6 caracteres
         if len(password) < 6:
             return render_template('usuarios/crear.html', error="La contraseña debe tener al menos 6 caracteres.")
         
-        # 4. Validar formato del email
+        # 3. Validar formato del email
         allowed_domains = ['gmail.com', 'hotmail.com', 'outlook.com']
         if not email.endswith(tuple(allowed_domains)):
             return render_template('usuarios/crear.html', error="El correo electrónico debe ser del dominio @gmail.com, @hotmail.com o @outlook.com.")
         
+        try:
+            nuevo_usuario = crear_usuario(nombre=nombre, email=email, password=password, rol=rol)    
+        except ValueError as e:
+            return render_template('usuarios/crear.html', error=str(e))
         return render_template('usuarios/crear.html', success="Usuario creado correctamente.")
     
     return render_template('usuarios/crear.html')
 
 @users_bp.route('/lista')
 def listar_usuarios():
-    # Aca podemos manejar la lógica para listar los usuarios
-    return render_template('usuarios/lista_usuarios.html')
+    usuarios = listar_usuarios()
+    return render_template('usuarios/lista_usuarios.html', usuarios=usuarios)
         
