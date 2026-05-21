f231d70de4f49dc75940a38afed81fc136e93f9c
Ian Kieferling <iankieferling@gmail.com>
2026-05-21 19:52:57 -0300

    Agregado atributo de intentos por codigo de verificacion + nullable=True en atributos que no se completan al crear un usuario como Administrador

--- src/core/usuarios/usuarios.py
+++ src/core/usuarios/usuarios.py
@@ -29,21 +29,22 @@ class Usuario(Base):
     
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     nombre: Mapped[str] = mapped_column(String(100), nullable=False)
-    apellido: Mapped[str] = mapped_column(String(100), nullable=False)
-    dni: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+    apellido: Mapped[str] = mapped_column(String(100), nullable=True)
+    dni: Mapped[str] = mapped_column(String(20), unique=True, nullable=True)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
-    direccion: Mapped[str] = mapped_column(String(255), nullable=False)
-    telefono: Mapped[str] = mapped_column(String(20), nullable=False)
-    fecha_nacimiento: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    direccion: Mapped[str] = mapped_column(String(255), nullable=True)
+    telefono: Mapped[str] = mapped_column(String(20), nullable=True)
+    fecha_nacimiento: Mapped[datetime] = mapped_column(DateTime, nullable=True)
     ruta_apto_fisico: Mapped[str] = mapped_column(String(255), nullable=True)
-    estado_apto_fisico: Mapped[EstadoAptoFisico] = mapped_column(Enum(EstadoAptoFisico), default=EstadoAptoFisico.SIN_CARGAR, nullable=False)
+    estado_apto_fisico: Mapped[EstadoAptoFisico] = mapped_column(Enum(EstadoAptoFisico), default=EstadoAptoFisico.SIN_CARGAR, nullable=True)
     
     rol: Mapped[RolUsuario] = mapped_column(Enum(RolUsuario), default=RolUsuario.CLIENTE, nullable=False)
-    estado: Mapped[EstadoUsuario] = mapped_column(Enum(EstadoUsuario), default=EstadoUsuario.PENDIENTE, nullable=False)
+    estado: Mapped[EstadoUsuario] = mapped_column(Enum(EstadoUsuario), default=EstadoUsuario.PENDIENTE, nullable=True)
 
     codigo_verificacion : Mapped[str] = mapped_column(String(6), nullable=True)
     codigo_verificacion_expira : Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    intentos_codigo: Mapped[int] = mapped_column(Integer, default=0, nullable=True)
     
     fecha_creacion: Mapped[datetime] = mapped_column(
         DateTime, default=lambda: datetime.now(tz_arg).replace(tzinfo=None),
