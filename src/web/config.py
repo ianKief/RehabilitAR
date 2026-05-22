6b74287e6eeb3086942c1060e091858dc6bfc18e
Ian Kieferling <iankieferling@gmail.com>
2026-05-22 20:04:58 -0300

    Agregadas las credenciales del mail

--- src/web/config.py
+++ src/web/config.py
@@ -27,6 +27,15 @@ class DevelopmentConfig:
         "default": f"{DB_SCHEME}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     }
 
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
+
 # Instancia única de configuración de desarrollo
 config = DevelopmentConfig
 
