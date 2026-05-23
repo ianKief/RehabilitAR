297164e7a9f06f80e43f16e02868616010d6e45f
Ian Kieferling <iankieferling@gmail.com>
2026-05-23 20:32:14 -0300

    Funciones para ver perfil y subir el apto fisico

--- src/web/controllers/usuarios.py
+++ src/web/controllers/usuarios.py
@@ -1,6 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from src.core.usuarios import crear_usuario as crear, listar_usuarios as listar
+import os
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session, request
+from src.core.usuarios import crear_usuario as crear, listar_usuarios as listar, obtener_usuario_por_id_core
+from src.core.usuarios.usuarios import EstadoAptoFisico
 from src.web.helpers.decorator import requiere_rol
+from datetime import datetime, timedelta
+from werkzeug.utils import secure_filename
+from src.core.database import db
 
 users_bp = Blueprint('usuarios', __name__, url_prefix='/usuarios')
 
@@ -42,4 +47,72 @@ def crear_usuario():
 def listar_usuarios():
     usuarios = listar()
     return render_template('usuarios/lista_usuarios.html', usuarios=usuarios)
+
+@users_bp.route('/perfil')
+def perfil():
+    user_id = session.get('usuario_id')
+    if not user_id:
+        flash("Debes iniciar sesion para ver tu perfil.", "warning")
+        return redirect(url_for('auth.login'))
+    
+    usuario = obtener_usuario_por_id_core(user_id)
+
+    if not usuario:
+        session.clear()
+        return redirect(url_for('auth.login'))
+    
+    dias_restantes = 0
+    if usuario.estado_apto_fisico and usuario.estado_apto_fisico.name == 'ACEPTADO' and usuario.fecha_apto_fisico:
+        fecha_vencimiento = usuario.fecha_apto_fisico + timedelta(days=365)
+        dias_restantes = (fecha_vencimiento - datetime.now()).days
+    
+    return render_template('usuarios/perfil.html', usuario=usuario, dias_restantes=dias_restantes)
+
+allowed_extensions = ('pdf', 'jpeg', 'jpg', 'png')
+@users_bp.route('/perfil/subir_apto', methods=['POST'])
+def subir_apto():
+    usuario_id = session.get('usuario_id')
+    if not usuario_id:
+        return redirect(url_for('auth.login'))
+    
+    usuario = obtener_usuario_por_id_core(usuario_id)
+    if not usuario:
+        return redirect(url_for('auth.login'))
+    
+    if usuario.estado_apto_fisico and usuario.estado_apto_fisico.name == 'ACEPTADO' and usuario.fecha_apto_fisico:
+        fecha_vencimiento = usuario.fecha_apto_fisico + timedelta(days=365)
+        if fecha_vencimiento > datetime.today():
+            flash("Ya posee un apto físico aprobado actualmente", "danger")
+            return redirect(url_for('usuarios.perfil'))
         
+    if 'apto_fisico' not in request.files:
+        flash("No se encontro ningun archivo.", "danger")
+        return redirect(url_for('usuarios.perfil'))
+
+    archivo = request.files.get('apto_fisico')
+    if archivo.filename == '':
+        flash("No seleccionaste ningún archivo", "warning")
+        return redirect(url_for('usuarios.perfil'))
+    
+    if not archivo.filename.lower().endswith(allowed_extensions):
+        flash("El tipo de archivo ingresado no es compatible", "danger")
+        return redirect(url_for('usuarios.perfil'))
+    
+    if archivo and archivo.filename != '':
+        nombre_archivo_apto = secure_filename(archivo.filename)
+        ruta_destino = os.path.join(os.getcwd(), 'src', 'web', 'static', 'uploads', 'aptos_fisicos')
+
+        os.makedirs(ruta_destino, exist_ok=True)
+
+        archivo.save(os.path.join(ruta_destino, nombre_archivo_apto))
+
+        usuario.ruta_apto_fisico = f"uploads/aptos_fisicos/{nombre_archivo_apto}"
+        usuario.estado_apto_fisico = EstadoAptoFisico.EN_REVISION
+        usuario.fecha_apto_fisico = datetime.today()
+
+        db.session.commit()
+
+        flash("Tu apto fisico fue subido exitosamente y esta pendiente de aprobacion", "success")
+        return redirect(url_for('usuarios.perfil'))
+
+    return redirect(url_for('usuarios.perfil'))
