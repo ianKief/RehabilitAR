36659677eba18841d7143ebabfed0e43cc58a711
Ian Kieferling <iankieferling@gmail.com>
2026-05-24 14:14:44 -0300

    Creacion por default de una cuenta de administrador

--- src/core/database.py
+++ src/core/database.py
@@ -25,6 +25,24 @@ def seed_db():
     """Pobla la base de datos con datos de prueba."""
     from src.core.seeds.salas_seeds import SalaSeeder
     from src.core.seeds.usuarios_seeds import UsuarioSeeder
+    from src.core.usuarios.usuarios import Usuario, RolUsuario, EstadoUsuario
+    from src.core.database import db
+
+    admin_existente = db.session.query(Usuario).filter_by(email='pruebasrehabilitar@gmail.com').first()
+    if not admin_existente:
+        admin = Usuario(
+            nombre = "Admin",
+            apellido = "Admin",
+            dni = "12345678",
+            email = "pruebasrehabilitar@gmail.com",
+            password = "123456",
+            rol = RolUsuario.ADMINISTRADOR,
+            estado = EstadoUsuario.ACTIVO
+        )
+        db.session.add(admin)
+        db.session.commit()
+        print("Admin creado con exito")
+        
     seeder = SalaSeeder(db)
     seeder.run()
     usuario_seeder = UsuarioSeeder(db)
