6d305194f0978b2b903b7ffd80412114da19ea82
AlfonArm <120581470+AlfonArm@users.noreply.github.com>
2026-05-24 13:56:15 -0300

    Eliminar objects que no se usan

--- src/web/objects/Alumno.py
+++
@@ -1,8 +0,0 @@
-class Alumno ():
-    def __init__(self, nombre, apellido, dni):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.dni = dni
-    
-    def nombreTieneTexto (self, texto):
-        return texto in self.nombre.lower() or texto in self.apellido.lower() or texto in self.dni.lower()
--- src/web/objects/AlumnoDetallado.py
+++
@@ -1,6 +0,0 @@
-from src.web.objects.Alumno import Alumno
-
-class AlumnoDetallado (Alumno):
-    def __init__(self, nombre, apellido, dni, porcentaje_asistencias):
-        super().__init__(nombre, apellido, dni)
-        self.porcentaje_asistencias = porcentaje_asistencias
