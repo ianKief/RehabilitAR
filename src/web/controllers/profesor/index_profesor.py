0c4a936378a60cae064082c9c6022b6464234274
AlfonArm <alfonsoarmanini@gmail.com>
2026-05-24 02:25:53 -0300

    Cambio de id_profesor pendiente del push previo

--- src/web/controllers/profesor/index_profesor.py
+++ src/web/controllers/profesor/index_profesor.py
@@ -7,7 +7,7 @@ from src.web.functions import devolver_hora_fin
 def renderizar_index_profesor ():
 
     id_profesor = session.get("id")
-    id_profesor = 4
+
     # Comprobación 1: el profesor está logeado [EN INSTANCIA]
     # COMPROBAR LOGIN
     #   flash('El profesor no está logueado', 'warning')
