efef96490a036c72e926bf9ddba7b7cd58f2efea
Ian Kieferling <iankieferling@gmail.com>
2026-05-29 20:38:32 -0300

    Correccion de error con send mail

--- src/core/usuarios/__init__.py
+++ src/core/usuarios/__init__.py
@@ -2,8 +2,6 @@ from sqlalchemy import func, text, or_, select, cast, Integer, and_, case
 from sqlalchemy.orm import aliased
 
 from flask_mail import Message
-from src.core.mail import send_mail
-#from src.web import mail
 
 from src.core.database import db
 from src.core.usuarios.usuarios import Usuario, RolUsuario, EstadoUsuario
@@ -220,6 +218,7 @@ def conseguir_administrativos ():
     return db.session.scalars(db.session.query(Administrador)).all()
 
 def informar_alta_demanda (clase):
+    from src.web import mail
     try:
         administrativos = conseguir_administrativos ()
         for admin in administrativos:
@@ -232,8 +231,7 @@ def informar_alta_demanda (clase):
                         recipients=[admin.email]
                     )
             msg.body = body
-            send_mail(msg)
-            #mail.send(msg)
+            mail.send(msg)
     except ValueError as e:
         clase.aviso_alta_demanda = False
 # Tampoco voy a informar al cliente del problema, mejor guardar el aviso para una próxima ocasión
