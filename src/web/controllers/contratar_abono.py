7babeedc909a1a9bfaed7d1b4b08a5960ed998c1
AlfonArm <120581470+AlfonArm@users.noreply.github.com>
2026-05-31 16:08:22 -0300

    Enlace grok actualizado
    en .env agregar URL_NGROK con en enlace

--- src/web/controllers/contratar_abono.py
+++ src/web/controllers/contratar_abono.py
@@ -36,10 +36,15 @@ def contratar_abono_route():
 
 
 #calcula el valor del abono y crea la preferencia de pago
-def calcular_contratacion_abono(dia_semana_elegido, sdk, user_id):
-    
+def calcular_contratacion_abono(dia_semana_elegido, descuento, sdk, user_id):
+    import os
+
+    URL = os.environ.get('URL_NGROK')
+
     #calcula el precio final del abono
     valor = calcular_valor_abono(dia_semana_elegido)
+
+    valor = valor * (1- descuento)
 
     #datos enviados a Mercado Pago
     preference_data = {
@@ -58,11 +63,11 @@ def calcular_contratacion_abono(dia_semana_elegido, sdk, user_id):
         },
 
         "back_urls": {
-            "success": "https://boss-daybed-chuck.ngrok-free.dev/contratar_abono/pago_exitoso",
-            "failure": "https://boss-daybed-chuck.ngrok-free.dev/contratar_abono/pago_fallido",
-            "pending": "https://boss-daybed-chuck.ngrok-free.dev/contratar_abono/pago_pendiente"
+            "success": f"{URL}/contratar_abono/pago_exitoso",
+            "failure": f"{URL}/contratar_abono/pago_fallido",
+            "pending": f"{URL}/contratar_abono/pago_pendiente"
         },
-        "notification_url": "https://boss-daybed-chuck.ngrok-free.dev/webhook"
+        "notification_url": f"{URL}/webhook"
     }
     
     #crea la preferencia de pago utilizando el SDK de Mercado Pago
