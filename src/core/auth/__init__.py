f8a57598ccd0001fb8188f377bc9f67172185c6c
Ian Kieferling <iankieferling@gmail.com>
2026-06-04 13:04:04 -0300

    fix: al registrarse con apto fisico, este aparece ahora como 'en revision' en lugar de 'sin subir'

--- src/core/auth/__init__.py
+++ src/core/auth/__init__.py
@@ -21,7 +21,7 @@ def registrar_cliente(nombre, apellido, dni, telefono, fecha_nacimiento, direcci
 
     nuevo_apto = None
     if nombre_archivo_apto:
-        nuevo_apto = AptoFisico(ruta_archivo=nombre_archivo_apto, fecha_carga=datetime.now(), estado=EstadoAptoFisico.SIN_CARGAR)
+        nuevo_apto = AptoFisico(ruta_archivo=nombre_archivo_apto, fecha_carga=datetime.now(), estado=EstadoAptoFisico.EN_REVISION)
     # Si todo está libre, creamos el usuario
     nuevo_cliente = Cliente(
         nombre=nombre,
