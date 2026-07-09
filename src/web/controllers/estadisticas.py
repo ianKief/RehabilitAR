d5d7dc56acb95e74b8b60d6c89b192b35073c7b7
YutaKoyanagi10 <yuta.koyanagi@alu.ing.unlp.edu.ar>
2026-07-08 23:28:56 -0300

    estadisticas de asistencia y suspendidos

--- src/web/controllers/estadisticas.py
+++ src/web/controllers/estadisticas.py
@@ -1,14 +1,24 @@
-from flask import Blueprint, current_app, flash,redirect,render_template,request,session,url_for
+from flask import Blueprint,render_template,request
 from src.web.helpers.decorator import requiere_rol
 from datetime import datetime
-from src.core.estadisticas import obtener_registros_mensuales,Mes,preparar_registros_para_grafico,obtener_anios_disponibles
+from src.core.estadisticas import (
+    obtener_registros_mensuales,
+    preparar_registros_para_grafico,
+    obtener_ganancias_mensuales,
+    preparar_ganancias_para_grafico,
+    obtener_asistencias_mensuales,
+    preparar_asistencias_para_grafico,
+    obtener_suspendidos_mensuales,
+    preparar_suspendidos_para_grafico,
+    obtener_anios_disponibles
+)
 
 bp = Blueprint ("estadisticas",__name__, url_prefix="/estadisticas")
 @bp.route("/",methods=["GET"])
 @requiere_rol(["ADMINISTRADOR"])
-
 def listar_estadisticas():
     anios = obtener_anios_disponibles()
+    is_search = request.args.get('is_search')
 
     anio = request.args.get(
         "anio",
@@ -16,15 +26,34 @@ def listar_estadisticas():
         type=int
     )
 
+    # Nuevos registros
     registros_db = obtener_registros_mensuales(anio)
-
-    registros,total_clientes = preparar_registros_para_grafico(registros_db, anio)
+    registros, total_clientes = preparar_registros_para_grafico(registros_db, anio)
     
+    # Ganancias
+    ganancias_db = obtener_ganancias_mensuales(anio)
+    ganancias, total_ganancias = preparar_ganancias_para_grafico(ganancias_db, anio)
+
+    # Asistencias
+    asistencias_db = obtener_asistencias_mensuales(anio)
+    asistencias, total_presentes, total_ausentes = preparar_asistencias_para_grafico(asistencias_db, anio)
+
+    # Suspendidos
+    suspendidos_db = obtener_suspendidos_mensuales(anio)
+    suspendidos, total_suspendidos = preparar_suspendidos_para_grafico(suspendidos_db, anio)
+
     return render_template(
         "estadisticas/listar.html",
         registros=registros,
-        anios=anios,
         total_clientes=total_clientes,
+        ganancias=ganancias,
+        total_ganancias=total_ganancias,
+        asistencias=asistencias,
+        total_presentes=total_presentes,
+        total_ausentes=total_ausentes,
+        suspendidos=suspendidos,
+        total_suspendidos=total_suspendidos,
+        anios=anios,
         anio_seleccionado=anio,
-        current_path=request.path
+        is_search=is_search
     )
